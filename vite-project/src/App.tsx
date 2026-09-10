682a05d57484f9c3318f51c3f279e43cfe1d1750
riad <anna21011975@gmail.com>
2026-09-10 10:20:21 +0300

    fdsfsgs

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <>
       <header>
-        <h1>Emoji Finger</h1>
+        <h1>Emoji Finder</h1>
         <p className='desc'>
           Find emoji by keywords
         </p>
@@ -27,10 +27,74 @@ function App() {
           <p className='title'>1234</p>
           <p className='keywords'>input symbol for numbers symbol</p>
         </div>
-
-
+        <div className='card'>
+          <p className='emoji'>🦀</p>
+          <p className='title'>Crab</p>
+          <p className='keywords'></p>
         </div>
-        
+        <div className='card'>
+          <p className='emoji'>✅</p>
+          <p className='title'>Check Mark</p>
+          <p className='keywords'></p>
+        </div>
+        <div className='card'>
+          <p className='emoji'>🫡</p>
+          <p className='title'>Saluting Face</p>
+          <p className='keywords'></p>
+        </div>
+        <div className='card'>
+          <p className='emoji'>🇺🇸</p>
+          <p className='title'>USA</p>
+          <p className='keywords'>Flag of USA</p>
+        </div>
+        <div className='card'>
+          <p className='emoji'>🥶</p>
+          <p className='title'>Cold</p>
+          <p className='keywords'>Cold, temperature, winter</p>
+        </div>
+        <div className='card'>
+          <p className='emoji'>🙅🏻‍♀️</p>
+          <p className='title'>Woman Gesturing No</p>
+          <p className='keywords'>No, stop, woman</p>
+        </div>
+        <div className='card'>
+          <p className='emoji'>👅</p>
+          <p className='title'>Tongue</p>
+          <p className='keywords'>Tongue</p>
+        </div>
+        <div className='card'>
+          <p className='emoji'>🙏🏿</p>
+          <p className='title'>Person With Folded Hands</p>
+          <p className='keywords'>Pls, thanks</p>
+        </div>
+        <div className='card'>
+          <p className='emoji'>🥳</p>
+          <p className='title'>Party Face</p>
+          <p className='keywords'>party, happy</p>
+        </div>
+        <div className='card'>
+          <p className='emoji'>🤬</p>
+          <p className='title'>Angry Face</p>
+          <p className='keywords'>Angry, mad, rage</p>
+        </div>
+        <div className='card'>
+          <p className='emoji'>💩</p>
+          <p className='title'>Poo</p>
+          <p className='keywords'>Poo, poop, shit</p>
+        </div>
+        <div className='card'>
+          <p className='emoji'>🤡</p>
+          <p className='title'>Clown</p>
+          <p className='keywords'>Clown, funny</p>
+        </div>
+        <div className='card'>
+          <p className='emoji'>💀</p>
+          <p className='title'>Skull</p>
+          <p className='keywords'>Skull, death</p>
+        </div>
+
+      </div>
+
       </main>
     </>
   )
